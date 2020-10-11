0c9111ccebf51c82abe11aab6469aef512f3601d
Gabriel Quispe <gabrielomarquisperosas@gmail.com>
2020-10-11 16:11:04 -0500

    admin component

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
-  styleUrls: ['./admin.component.scss']
+  styleUrls: ['./admin.component.scss'],
 })
-export class AdminComponent implements OnInit {
+export class AdminComponent {
+  public toggleValue: boolean;
 
-  constructor() { }
+  public constructor(private cd: ChangeDetectorRef) {}
 
-  ngOnInit(): void {
+  public toggle(ev) {
+    this.toggleValue = ev;
+    this.cd.detectChanges();
   }
-
 }
--- src/app/admin/core/components/header/header.component.ts
+++ src/app/admin/core/components/header/header.component.ts
@@ -1,12 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-admin-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent implements OnInit {
-  constructor() {}
+export class HeaderComponent {
+  @Output() public toggleMenu: EventEmitter<boolean> = new EventEmitter<
+    boolean
+  >();
+  public toggledValue: boolean;
+  public constructor() {
+    this.toggledValue = true;
+  }
 
-  ngOnInit(): void {}
+  public toggleSidebar(): void {
+    this.toggledValue = !this.toggledValue;
+    this.toggleMenu.emit(this.toggledValue);
+  }
 }
